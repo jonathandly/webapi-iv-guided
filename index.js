fed743991c7cd3b2052b1a883a8556c8f29da2df
Jonathan Daly <dalyj138@gmail.com>
2019-05-15 22:26:36 -0400

    Ready for deployment.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// Read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
